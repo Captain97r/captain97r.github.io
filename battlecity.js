7ef19ffc5e0d8f03e48c807ab71757c081a97ac1
Evgeny Radchenko <captain.r97@gmail.com>
2022-01-24 21:17:36 +0300

    Added smooth object motion

--- battlecity.js
+++ battlecity.js
@@ -4,7 +4,7 @@ spriteSheet.src = "img/sprite.bmp";
 var ctx = document.getElementById('drawingCanvas').getContext('2d');
 var tank = new GameObject(spriteSheet, 0, 0, 32, 32);
 
-const Direction = { LEFT: 1, RIGHT: 2, UP: 3, DOWN: 4 };
+const Direction = { NONE: 0, LEFT: 1, RIGHT: 2, UP: 3, DOWN: 4 };
 
 function GameObject(spriteSheet, x, y, width, height) {
     this.spriteSheet = spriteSheet;
@@ -15,52 +15,82 @@ function GameObject(spriteSheet, x, y, width, height) {
 
     this.posX = 0;
     this.posY = 0;
+    this.speed = 3;
+    this.direction = 0;
+
+    this.setDirection = function(direction) {
+        this.direction = direction;
+    }
+
+    this.update = function() {
+        switch(this.direction) {
+            case Direction.DOWN:
+                this.posY+=this.speed;
+                break;
+            case Direction.UP:
+                this.posY-=this.speed;
+                break;
+            case Direction.LEFT:
+                this.posX-=this.speed;
+                break;
+            case Direction.RIGHT:
+                this.posX+=this.speed;
+                break;
+        }
+    }
 
     this.draw = function(context) {
         context.drawImage(this.spriteSheet, this.x, this.y, this.width, this.height, this.posX, this.posY, this.width, this.height);
     }
 
-    this.move = function(direction) {
-        switch(direction) {
-            case Direction.DOWN:
-                this.posY++;
-                break;
-            case Direction.UP:
-                this.posY--;
-                break;
-            case Direction.LEFT:
-                this.posX--;
-                break;
-            case Direction.RIGHT:
-                this.posX++;
-                break;
-        }
+}
+
+function handleKeyPress(evt) {
+    switch(evt.code) {
+        case "KeyW":
+            tank.setDirection(Direction.UP);
+            break;
+        case "KeyS":
+            tank.setDirection(Direction.DOWN);
+            break;
+        case "KeyA":
+            tank.setDirection(Direction.LEFT);
+            break;
+        case "KeyD":
+            tank.setDirection(Direction.RIGHT);
+            break;
     }
 }
 
-function handleKeyPress(evt)
-{
+function handleKeyRelease(evt) {
     switch(evt.code) {
         case "KeyW":
-            tank.move(Direction.UP);
-            break;
         case "KeyS":
-            tank.move(Direction.DOWN);
-            break;
         case "KeyA":
-            tank.move(Direction.LEFT);
-            break;
         case "KeyD":
-            tank.move(Direction.RIGHT);
+            tank.setDirection(Direction.NONE);
             break;
     }
 }
 
 function go() {
+    tank.update();
     tank.draw(ctx);
 }
 
 window.onload = function () {
     document.addEventListener("keydown", handleKeyPress);
-    setInterval(go, 1000/15);
+    document.addEventListener("keyup", handleKeyRelease);
+
+    var lastUpdateTime = (new Date()).getTime();
+
+    // setInterval(function() {
+    //     go();
+    //     var currentTime = (new Date()).getTime();
+    //     var timeDifference = currentTime - lastUpdateTime;
+    //     player.x += 5 * timeDifference;
+    //     lastUpdateTime = currentTime;
+    //   }, 1000 / 60);
+
+    setInterval(go, 10);
 }
